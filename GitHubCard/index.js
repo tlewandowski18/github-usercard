663dc5f2d80a50519320c9010c26c122976641fd
tlewandowski18 <tlewandowski18@gmail.com>
2019-12-11 23:22:33 -0500

    Create component function, request data from github apis for my own profile and follower/instructor profiles

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -53,3 +53,95 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+const cards = document.querySelector('.cards');
+
+const instructorList = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell']
+
+axios 
+  .get('https://api.github.com/users/tlewandowski18')
+  .then((res) => {
+    cards.appendChild(createCard(res.data));
+  })
+  .catch((err) => {
+    return `You have an error! ${err}`
+  })
+
+instructorList.forEach(instructor => {
+  axios 
+  .get(`https://api.github.com/users/${instructor}`)
+  .then((res) => {
+    cards.appendChild(createCard(res.data));
+  })
+  .catch((err) => {
+    return `You have an error! ${err}`
+  })
+})
+
+axios
+  .get('https://api.github.com/users/tlewandowski18/followers')
+  .then((res) => {
+    followerList = res.data
+    followerList.forEach(follower => {
+      cards.appendChild(createCard(follower));
+    })
+  }) 
+
+function createCard(obj) {
+  const newCard = document.createElement('div');
+  const cardImg = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const username = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const githubAddress = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+
+  newCard.appendChild(cardImg);
+  newCard.appendChild(cardInfo);
+
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  profile.appendChild(githubAddress);
+
+  newCard.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  username.classList.add('username');
+
+  cardImg.src = obj.avatar_url;
+
+  name.textContent = obj.name;
+  username.textContent = obj.login;
+  location.textContent = `Location: ${obj.location}`
+  profile.innerHTML = `Profile: <a href='${obj.html_url}'>${obj.html_url}</a>`;
+  followers.textContent = `Followers: ${obj.followers}`;
+  following.textContent = `Following: ${obj.following}`;
+  bio.textContent = `Bio: ${obj.bio}`;
+
+  return newCard;
+}
+
+/* <div class="card">
+  <img src={image url of user} />
+  <div class="card-info">
+    <h3 class="name">{users name}</h3>
+    <p class="username">{users user name}</p>
+    <p>Location: {users location}</p>
+    <p>Profile:  
+      <a href={address to users github page}>{address to users github page}</a>
+    </p>
+    <p>Followers: {users followers count}</p>
+    <p>Following: {users following count}</p>
+    <p>Bio: {users bio}</p>
+  </div>
+</div> */
